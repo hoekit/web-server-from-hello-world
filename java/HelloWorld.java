226b3772c66c82dd34fc526560b91dcce45bf008
Hoe Kit CHEW <hoekit@gmail.com>
2021-12-30 11:27:44 +0700

    Initial java version

---
+++ java/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
+
+/*
+# This: HelloWorld.java
+# Prev: -
+# Next: 
+
+# Build & run:
+JNAME=HelloWorld; javac ${JNAME}.java && java ${JNAME}
+*/
+
